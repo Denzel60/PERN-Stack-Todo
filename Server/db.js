dcc0bdbfc8a86037e2be1587ee379f0f98d923e7
Denzel <lawrencedenzel60@gmail.com>
2024-07-13 21:25:03 +0300

    "Updated .env file and db.js to change PostgreSQL password and removed environment variable references"

--- Server/db.js
+++ Server/db.js
@@ -1,18 +1,18 @@
-const Pool = require('pg').Pool
-require('dotenv').config()
+const Pool = require("pg").Pool;
+require("dotenv").config();
 
 const pool = new Pool({
-    // user: process.env.USERNAME,
-    // host: process.env.HOST,
-    // password: process.env.PASSWORD,
-    // database: 'todoapp',
-    // port: process.env.DBPORT
+  // user: process.env.USERNAME,
+  // host: process.env.HOST,
+  // password: process.env.PASSWORD,
+  // database: 'todoapp',
+  // port: process.env.DBPORT
 
-    user: 'postgres',
-    host: 'localhost',
-    password: '1974',
-    database: 'todoapp',
-    port: 5432
-})
+  user: "postgres",
+  host: "localhost",
+  password: "00000000",
+  database: "todoapp",
+  port: 5432,
+});
 
-module.exports = pool
+module.exports = pool;
